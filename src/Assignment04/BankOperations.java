81e538aae5f46ad6a2fdc3c2217b14de565423c0
mohbes <mohbes@IMM.INTRA>
2016-10-05 11:36:06 +0200

    version1 crating bankaccount system

---
+++ src/Assignment04/BankOperations.java
@@ -0,0 +1,17 @@
+package Assignment04;
+
+
+
+public interface BankOperations {
+	public void withdrawMoney(double amount);
+	public void depositMoney(double amount);
+
+}
+
+
+/**
+public interface BankOperations {
+	public void withdrawMoney(double amount);
+	public void depositMoney(double amount);
+
+}*/
